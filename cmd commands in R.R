dee3c4f03fb697f21ff459b8be3b6162090ae540
tomaztk <tomaz.kastrun@gmail.com>
2020-01-05 15:57:57 +0100

    update of file

--- cmd commands in R.R
+++ cmd commands in R.R
@@ -10,3 +10,42 @@
 # Date: 20200105
 #
 #####################################
+
+
+ ## 1 Simple dir command with order switch
+
+system("dir /o")
+system2("dir /o")
+shell("dir /o")
+shell.exec("dir /o")
+
+#both return the result to console pane
+system("ping google.com")
+shell("ping google.com -n 1")
+
+
+
+## 2 Chaining  cmd commands in R
+setwd("C:\\Users\\Tomaz")
+
+### not work
+shell("dir")
+shell("cd ..")
+shell("dir")
+
+### will work
+shell("dir && cd .. &&  dir")
+shell("dir & cd .. &  dir")
+
+
+### adding echo
+shell ("echo hello && dir && echo by by")
+shell ("echo You ran the command at:  %TIME%  on: %DATE% && dir")
+
+
+### checking the environment variables
+getwd()
+Sys.getenv(c("R_HOME","HOME"))
+
+
+## 3 Typical CMD commands
